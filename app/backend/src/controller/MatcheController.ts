7a0a56aa56588fd082945546b6fa7dca3983c49a
devpedroval <prmval@gmail.com>
2023-07-17 15:04:36 -0300

    teste 04 passando

--- app/backend/src/controller/MatcheController.ts
+++ app/backend/src/controller/MatcheController.ts
@@ -5,6 +5,12 @@ export default class MatcheController {
   constructor(private matcheService = new MatcheService()) {}
 
   async getAllMatches(req: Request, res: Response): Promise<Response> {
+    if (req.query.inProgress) {
+      const matches = await this.matcheService.getMatchesByProgress(
+        req.query.inProgress === 'true',
+      );
+      return res.status(matches.status).json(matches.data);
+    }
     const matches = await this.matcheService.getAllMatches();
     return res.status(matches.status).json(matches.data);
   }
--- app/backend/src/service/MatcheService.ts
+++ app/backend/src/service/MatcheService.ts
@@ -4,11 +4,9 @@ import IResponse from '../Interfaces/IResponse';
 
 export default class MatcheService {
   constructor(private matchesModel = MatchesModel) {}
-  async getAllMatches(): Promise<IResponse> {
-    const matches = await this.matchesModel.findAll({
-      include: ['homeTeam', 'awayTeam'],
-    });
-    const response = matches.map((match) => {
+
+  private static mappingResponse(matches: MatchesModel[]) {
+    return matches.map((match) => {
       const eachMatchResponse = match.toJSON() as IMatchResponse;
       return {
         ...eachMatchResponse,
@@ -16,11 +14,27 @@ export default class MatcheService {
         awayTeam: { teamName: eachMatchResponse.awayTeam.teamName },
       };
     });
+  }
+
+  async getAllMatches(): Promise<IResponse> {
+    const matches = await this.matchesModel.findAll({
+      include: ['homeTeam', 'awayTeam'],
+    });
+
     return {
       status: 200,
-      data: response,
+      data: MatcheService.mappingResponse(matches),
     };
   }
 
-  // async getMatchesByProgress(inProgress: boolean): Promise<IResponse> {}
+  async getMatchesByProgress(inProgress: boolean): Promise<IResponse> {
+    const matches = await this.matchesModel.findAll({
+      where: { inProgress },
+      include: ['homeTeam', 'awayTeam'],
+    });
+    return {
+      status: 200,
+      data: MatcheService.mappingResponse(matches),
+    };
+  }
 }
